86751e06fc2c9868a3f9bff7269bc0a4f19f9600
suchijha <94467537+suchijha@users.noreply.github.com>
2024-08-09 09:08:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
 let password=" ";
 	for(let i=1;i<=l;i++){
     const random = Math.floor(Math.random() * strLength);
-		password=password+str[rand];
+		password=password+str[random];
 		
 	}
 return password;
